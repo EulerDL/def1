355a3952664e101f36012b14b640e5e9aef0b633
trkdmtr <trkdmtr@gmail.com>
2022-01-23 14:00:14 +0300

    Fix benchmark

--- benchmark.py
+++ benchmark.py
@@ -1,13 +1,26 @@
+from json import load
 import sys
+import time
 from PIL import Image
 import numpy as np
 import torch
 from torchvision.transforms.functional import to_tensor
 
-from Training import pixel_accuracy, mIoU
+from Data import load_img
+from Training_functions import pixel_accuracy, mIoU
+from Simple_Unet import *
 
-img = torch.load(sys.argv[1])
+data = None
+with open(sys.argv[1],'rb') as file:
+    data = file.read()
+img = load_img(data)
 mask = to_tensor(np.load(sys.argv[2]))
-acc = pixel_accuracy(img,mask)
-iou = mIoU(img,mask,5)
-print('Accuracy: {}, IoU: {}'.format(acc,iou))
+model = UNet(num_class=7)
+model.load_state_dict(torch.load('dict.pth'))
+model.eval()
+start = time.perf_counter()
+res = model(img)
+end = time.perf_counter()
+acc = pixel_accuracy(res,mask)
+iou = mIoU(res,mask,7)
+print('Accuracy: {}, IoU: {}, time elapsed: {}'.format(acc,iou,end-start))
