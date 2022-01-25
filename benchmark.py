2e16781498699af87f603507189f312fd6d3fb5d
trkdmtr <trkdmtr@gmail.com>
2022-01-25 12:19:34 +0300

    Fix benchmark

--- benchmark.py
+++ benchmark.py
@@ -1,22 +1,27 @@
 from json import load
 import sys
 import time
-from PIL import Image
 import numpy as np
 import torch
 from torchvision.transforms.functional import to_tensor
+from cv2 import resize
 
-from Data import load_img
+from Data import ImageIO
 from Training_functions import pixel_accuracy, mIoU
 from Simple_Unet import *
 
 data = None
 with open(sys.argv[1],'rb') as file:
     data = file.read()
-img = load_img(data)
-mask = to_tensor(np.load(sys.argv[2]))
+img_io = ImageIO()
+img = img_io.load(data)
+mask = np.load(sys.argv[2])
+shape = img_io.w,img_io.h
+mask = resize(mask,shape)
+mask = to_tensor(mask)
 model = UNet(num_class=7)
-model.load_state_dict(torch.load('dict.pth'))
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model.load_state_dict(torch.load('dict.pth', map_location=device))
 model.eval()
 start = time.perf_counter()
 res = model(img)
